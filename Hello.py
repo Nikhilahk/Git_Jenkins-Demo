dd36f7b1c23f61725cd32d8a0caae9f692bd0b22
NikhilaDover <1ms17ee067@gmail.com>
2022-04-20 16:36:22 +0530

    program for addition is added

--- Hello.py
+++ Hello.py
@@ -4,8 +4,18 @@ print ("Hello World!")
 
 print("This is Nikhila")
 
-print("Editing to merge in master")
+print("Writing a program to add 2 numbers")
 
-print("2")
+num1 = 2
+num2 = 4
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print("num1",num1)
+print("num2",num2)
+
+print("sum of num1 and num2 is " sum)
 
 
